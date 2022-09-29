4c0bee70869337bc29fd0875386132c88c618759
dewibsml <112691180+dewibsml@users.noreply.github.com>
2022-09-29 13:07:34 +0700

    Create  Pnydewi.js

---
+++ P04-css responsive/Pnydewi.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('.top').click(function(){
+      let isi = $(this).text();
+      let tid = $(this).prop('id');
+      let rid = tid.split('__');
+      let id_baris = rid[1];
+      let nama = $('#nama__'+id_baris).text();
+
+      if(isi=='Hapus'){
+          let konfirmasi = confirm(` Yakin ingin menghapus..! ${nama} ??`);
+          if(!konfirmasi) return;
+
+          $('#fade__'+id_baris).fadeOut();
+
+      }else{
+          alert("Ente milih : " + $(this).html() + "!");
+      }
+  })
+})
