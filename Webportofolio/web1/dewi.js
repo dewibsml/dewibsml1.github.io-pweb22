a1344ac26c2be585f857d8fc3b54c5c379ead515
dewibsml <112691180+dewibsml@users.noreply.github.com>
2022-09-30 17:36:58 +0700

    Update dewi.js

--- Webportofolio/web1/dewi.js
+++ Webportofolio/web1/dewi.js
@@ -3,3 +3,30 @@ $(document).ready(function(){
         $("#panel").slideToggle("slow");
     });
 });
+
+$('#menu li a').removeClass('active');
+      $(this).addClass('active');
+      
+      var href = $(this).attr('href');
+      $('#About>div').hide();
+      
+      $(href).show();
+            
+      if(currentSection == 0){
+          $('#seleksi').slideToggle(200);
+          currentSection = href;
+      }else if(currentSection == href){
+           $('#seleksi').slideToggle(200);
+           currentSection = 0;    
+      }else{
+          currentSection = href;
+      }
+      return false;
+  });
+});
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
