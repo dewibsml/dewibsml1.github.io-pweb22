014a24b49280240de0d83bb2008fba90bf240436
dewibsml <112691180+dewibsml@users.noreply.github.com>
2022-10-02 00:12:37 +0700

    Update script.js

--- Webportofolio/js/script.js
+++ Webportofolio/js/script.js
@@ -23,3 +23,24 @@ let words = '';
 	setTimeout(ngetik, 500);
 
 })();
+
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideToggle("slow");
+    });
+});
+
+    $("#tugas").click(function(){
+        $("#pweb").slideToggle("slow");
+    });
+});
+
+    $("#quotes").click(function(){
+        $("#nimal").slideToggle("slow");
+    });
+});
+
+    $("#contact").click(function(){
+        $("#cinta").slideToggle("slow");
+    });
+});
